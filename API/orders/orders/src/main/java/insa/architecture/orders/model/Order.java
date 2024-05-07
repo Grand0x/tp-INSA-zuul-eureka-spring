11a6fb8f6e36355a454f556655e90c1fb84cd545
max g <gmaxg02@gmail.com>
2024-05-07 11:43:04 +0200

    Eureka service

--- API/orders/orders/src/main/java/insa/architecture/orders/model/Order.java
+++ API/orders/orders/src/main/java/insa/architecture/orders/model/Order.java
@@ -1,6 +1,6 @@
 package insa.architecture.orders.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
@@ -88,52 +88,3 @@ public class Order {
     }
 }
 
-@Entity
-@Table(name = "order_items")
-public class OrderItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "order_id", nullable = false)
-    private Order order;
-
-    @Column(name = "product_id")
-    private Long productId;
-
-    private Integer quantity;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-}
--- API/orders/orders/src/main/java/insa/architecture/orders/repository/OrderItemRepository.java
+++ API/orders/orders/src/main/java/insa/architecture/orders/repository/OrderItemRepository.java
@@ -1,14 +1,9 @@
 package insa.architecture.orders.repository;
 
-import insa.architecture.orders.model.Order;
 import insa.architecture.orders.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByClientId(Long clientId);
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
-
